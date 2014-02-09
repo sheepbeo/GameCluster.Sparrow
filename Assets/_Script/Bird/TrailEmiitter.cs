74b37e692b9eb54424d6b9bfc50bad49f16a56de
sheepbeo <lequoccuong91@yahoo.com>
2014-02-09 15:47:22 +0200

    add trail emitter

---
+++ Assets/_Script/Bird/TrailEmiitter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailEmiitter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.parent.rigidbody.velocity.magnitude < 0.1f) {
+			particleSystem.emissionRate = 0;
+		} else {
+			particleSystem.emissionRate = 100;
+		}
+	}
+}
