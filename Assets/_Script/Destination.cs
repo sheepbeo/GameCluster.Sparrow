bab2e10bac3f605b7a58f5dd9d1eb940c9811b34
sheepbeo <lequoccuong91@yahoo.com>
2014-02-21 18:23:00 +0200

    add colliders to tree and swallow add destination trigger win msg and score

---
+++ Assets/_Script/Destination.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destination : MonoBehaviour {
+	public Transform birdTransform;
+
+	private bool isDestinationReached = false;
+	private BirdManager birdManager;
+
+	// Use this for initialization
+	void Start () {
+		birdManager = birdTransform.GetComponent<BirdManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("BirdCollider")) {
+			isDestinationReached = true;
+		}
+	}
+
+	void OnGUI() {
+		if (isDestinationReached) {
+			GUI.Label(new Rect(100,80,100,100), "You Win!");
+			GUI.Label(new Rect(100,100,100,100), "Score: " + birdManager.Score);
+		}
+	}
+}
---
+++ Assets/_Script/TreeManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeManager : MonoBehaviour {
+	public Terrain terrain;
+	public GameObject[] CollidersPrefab;
+
+	private TreeInstance[] treeInstances;
+	private TreePrototype[] treePrototypes;
+
+	// Use this for initialization
+	void Start () {
+		treePrototypes = terrain.terrainData.treePrototypes;
+		treeInstances = terrain.terrainData.treeInstances;
+
+		foreach (TreeInstance treeInst in treeInstances) {
+			foreach (GameObject colliPrefb in CollidersPrefab) {
+				if (treePrototypes[treeInst.prototypeIndex].prefab.CompareTag(colliPrefb.tag)) {
+					Vector3 colliderPos = treeInst.position;
+					colliderPos.x *= terrain.terrainData.size.x;
+					colliderPos.y *= terrain.terrainData.size.y;
+					colliderPos.z *= terrain.terrainData.size.z;
+					colliderPos += terrain.transform.position;
+
+					Instantiate(colliPrefb, colliderPos, Quaternion.identity);
+				}
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
